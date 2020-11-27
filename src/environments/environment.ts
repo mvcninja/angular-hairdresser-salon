239bb87219c885ecc618e5f2825685bf872b6ae8
Mark Cooke <mvcninja@texashomeresources.com>
2020-11-27 14:59:37 -0600

    second commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAz9Px0aQIAuj6hK73lGoGtSx8OTYYiml4',
+    authDomain: 'my-hairdresser-salon-b353b.firebaseapp.com',
+    databaseURL: 'https://my-hairdresser-salon-b353b.firebaseio.com',
+    projectId: 'my-hairdresser-salon-b353b',
+    storageBucket: 'my-hairdresser-salon-b353b.appspot.com',
+    messagingSenderId: '680876995296',
+    appId: '1:680876995296:web:635787d69d03a8e6b99685',
+    measurementId: 'G-YBRJBW24Q9'
+  }
 };
 
 /*
@@ -13,4 +23,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
+import 'zone.js/dist/zone-error';  // Included with Angular CLI.
